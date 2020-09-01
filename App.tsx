55764c7ca98d6ae73d43530a13a928034c4fb96d
Pablo Medina <pablomed88@gmail.com>
2020-09-01 11:52:55 -0300

    Setting redux.

--- App.tsx
+++ App.tsx
@@ -6,10 +6,16 @@ import AppNavigator from './src/navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { navigationRef } from './src/navigation/RootNavigation';
 
+// REDUX
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+
 function App() {
   return (
     <NavigationContainer ref={navigationRef}>
-      <AppNavigator />
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
     </NavigationContainer>
   );
 }
