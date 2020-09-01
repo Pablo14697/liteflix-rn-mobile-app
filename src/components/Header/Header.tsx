7a9d7bd93180a40e688937f8a1401bb0824c9c41
Pablo Medina <pablomed88@gmail.com>
2020-09-01 19:14:13 -0300

    Added goBack function to arrow back on header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 
 // ASSETS
 import { ArrowBack } from '../../assets/images';
+
+// STYLES
 import { ArrowBackButton, Fill, HeaderContainer, NativeStyles } from './styles';
+
+// COMPONENTS
 import Typography from '../Typography/Typography';
+
+// NAVIGATION
+import { goBack } from '../../navigation';
 
 interface Props {
   title: string;
@@ -12,7 +19,7 @@ interface Props {
 
 const Header = ({ title }: Props) => (
   <HeaderContainer>
-    <ArrowBackButton hitSlop={NativeStyles.hitSlop}>
+    <ArrowBackButton hitSlop={NativeStyles.hitSlop} onPress={goBack}>
       <ArrowBack height={25} width={35} />
     </ArrowBackButton>
     <Typography color="white" size={20}>
