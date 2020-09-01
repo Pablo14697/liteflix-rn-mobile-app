83345e099b2d4e387106f6e28920a20f6cb96e4a
Pablo Medina <pablomed88@gmail.com>
2020-09-01 11:26:13 -0300

    Env variables were set up.

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+import envVariables from 'react-native-config';
+
+const { COMING_SOON_API_URL, OUTSTANDING_API_URL, NODE_ENV, POPULAR_API_URL } = envVariables;
+
+export const isProduction = NODE_ENV === 'prod';
+
+interface Config {
+  COMING_SOON_API_URL: string;
+  OUTSTANDING_API_URL: string;
+  POPULAR_API_URL: string;
+}
+
+const Config = {
+  COMING_SOON_API_URL,
+  OUTSTANDING_API_URL,
+  POPULAR_API_URL,
+};
+
+export default Config;
